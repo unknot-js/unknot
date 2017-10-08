c2588ae88160be9fab6e859038ec23fd204c2a8c
Jack Jennings <jack@standard-library.com>
2017-10-07 19:31:23 -0700

    Improve missing element error message

--- build/errors.js
+++ build/errors.js
@@ -3,9 +3,19 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+var notFoundMessage = function notFoundMessage(name, selector) {
+  return "\u2B07\u2B07\u2B07\nunknot: " + name + "\n\nCannot find an element matching `" + selector + "` in the document.\n\nIf the element that you are trying to query may not be present, you can use the \"maybe\" function instead:\n\n  $.maybe(\"" + selector + "\")";
+};
+
 function NotFoundException(selector) {
+  var _this = this;
+
   this.name = "NotFoundException";
-  this.message = "An element matching the selector \"" + selector + "\" was expected, but not found in the document.\n\nIf the element that you are trying to query may not be present, you can use the \"maybe\" function instead:\n\n  $.maybe(\"" + selector + "\")";
+  this.message = notFoundMessage(this.name, selector);
+
+  this.toString = function () {
+    return _this.message;
+  };
 }
 
 exports.NotFoundException = NotFoundException;
---
+++ test/errors.js
@@ -0,0 +1,20 @@
+import test from "tape";
+import { Kefir as K } from "kefir";
+
+import { NotFoundException } from "../lib/errors";
+
+test("NotFoundException returns a descriptive message", t => {
+  const error = new NotFoundException(".asdf");
+  const expected = `\u2B07\u2B07\u2B07
+unknot: NotFoundException
+
+Cannot find an element matching \`.asdf\` in the document.
+
+If the element that you are trying to query may not be present, you can use the "maybe" function instead:
+
+  $.maybe(".asdf")`;
+
+  t.equal(expected, error.message);
+  t.equal(expected, error.toString());
+  t.end();
+});
